06b98eb68b9f5f910b479e51399f18ea054ef6ef
yjosuesilva-code <yjosuesilva@unicesar.edu.co>
2026-04-16 00:33:51 -0500

    refactor: Se dividio la clase Datos con la clase Carta

--- RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Carta.java
+++ RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Carta.java
@@ -1,4 +1,8 @@
 package com.mycompany.restauranteelbuensabor;
 
 public class Carta {
+    public static String[] nombres = {"Bandeja Paisa", "Sancocho de Gallina", "Arepa con Huevo", "Jugo Natural", "Gaseosa", "Cerveza Poker", "Agua Panela", "Arroz con Pollo"};
+    public static double[] precios = {32000, 28000, 8000, 7000, 4500, 6000, 3500, 25000};
+    public static String nombreRestaurante = "El Buen Sabor";
+    public static int numeroFactura = 1;
 }
--- RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Datos.java
+++ RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Datos.java
@@ -9,13 +9,9 @@ package com.mycompany.restauranteelbuensabor;
  * @author alfre
  */
 public class Datos {
-    public static String[] nombres = {"Bandeja Paisa", "Sancocho de Gallina", "Arepa con Huevo", "Jugo Natural", "Gaseosa", "Cerveza Poker", "Agua Panela", "Arroz con Pollo"};
-    public static double[] precios = {32000, 28000, 8000, 7000, 4500, 6000, 3500, 25000};
     public static int[] cantidades = {0, 0, 0, 0, 0, 0, 0, 0};
     public static int numeroMesaActual = 0;
     public static int estadoMesa = 0;
     public static double total = 0;
-    public static int numeroFactura = 1;
-    public static String nombreRestaurante = "El Buen Sabor";
     public static String tmp = "";
 }
