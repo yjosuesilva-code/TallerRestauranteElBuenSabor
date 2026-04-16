bcce80fc4493e8ab39fbd45d8e3146918604f073
yjosuesilva-code <yjosuesilva@unicesar.edu.co>
2026-04-16 00:52:22 -0500

    refactor: Se modifico los acceso a los atributos de cada clase despues de eliminar Datos

--- RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Imprimir.java
+++ RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Imprimir.java
@@ -15,8 +15,8 @@ public class Imprimir {
         System.out.println("    --- NUESTRA CARTA ---");
         System.out.println("========================================");
         int i = 0;
-        while (i < Datos.nombres.length) {
-            System.out.printf("%d. %-22s $%,.0f%n", (i + 1), Datos.nombres[i], Datos.precios[i]);
+        while (i < Carta.nombres.length) {
+            System.out.printf("%d. %-22s $%,.0f%n", (i + 1), Carta.nombres[i], Carta.precios[i]);
             i++;
         }// fin while
         System.out.println("========================================");
@@ -26,12 +26,12 @@ public class Imprimir {
         double sub = 0;
         int i = 0;
         System.out.println("--- PEDIDO ACTUAL ---");
-        while (i < Datos.nombres.length) {
-            if (Datos.cantidades[i] > 0) {
+        while (i < Carta.nombres.length) {
+            if (PedidoActual.cantidades[i] > 0) {
                 // imprime producto con cantidad y subtotal parcial
-                System.out.printf("%-20s nombreRestaurante%-6d $%,.0f%n", Datos.nombres[i], Datos.cantidades[i], (Datos.precios[i] * Datos.cantidades[i]));
+                System.out.printf("%-20s nombreRestaurante%-6d $%,.0f%n", Carta.nombres[i], PedidoActual.cantidades[i], (Carta.precios[i] * PedidoActual.cantidades[i]));
                 // suma al subtotal
-                sub = sub + Datos.precios[i] * Datos.cantidades[i];
+                sub = sub + Carta.precios[i] * PedidoActual.cantidades[i];
             }
             i++;
         }// fin while
@@ -48,9 +48,9 @@ public class Imprimir {
         double aux = 0;
         // calcula subtotal otra vez
         int i = 0;
-        while (i < Datos.nombres.length) {
-            if (Datos.cantidades[i] > 0) {
-                sub = sub + Datos.precios[i] * Datos.cantidades[i];
+        while (i < Carta.nombres.length) {
+            if (PedidoActual.cantidades[i] > 0) {
+                sub = sub + Carta.precios[i] * PedidoActual.cantidades[i];
                 cont = cont + 1;
             }
             i++;
@@ -76,13 +76,13 @@ public class Imprimir {
         System.out.println("    Calle 15 #8-32, Valledupar");
         System.out.println("    NIT: 900.123.456-7");
         System.out.println(sep);
-        System.out.printf("FACTURA No. %03d%n", Datos.numeroFactura);
+        System.out.printf("FACTURA No. %03d%n", Carta.numeroFactura);
         System.out.println("----------------------------------------");
         // imprime cada item del pedido
         int j = 0;
-        while (j < Datos.nombres.length) {
-            if (Datos.cantidades[j] > 0) {
-                System.out.printf("%-20s nombreRestaurante%-6d $%,.0f%n", Datos.nombres[j], Datos.cantidades[j], (Datos.precios[j] * Datos.cantidades[j]));
+        while (j < Carta.nombres.length) {
+            if (PedidoActual.cantidades[j] > 0) {
+                System.out.printf("%-20s nombreRestaurante%-6d $%,.0f%n", Carta.nombres[j], PedidoActual.cantidades[j], (Carta.precios[j] * PedidoActual.cantidades[j]));
             }
             j++;
         }// fin while
@@ -99,9 +99,9 @@ public class Imprimir {
         System.out.println("El Buen Sabor - Valledupar");
         System.out.println(sep);
         // actualiza estado e incrementa factura - tres responsabilidades en un metodo
-        Datos.numeroFactura = Datos.numeroFactura + 1;
-        Datos.estadoMesa = 0;
-        Datos.total = tot;
+        Carta.numeroFactura = Carta.numeroFactura + 1;
+        Mesa.estadoMesa = 0;
+        PedidoActual.total = tot;
     }
 
     public static void imprimirFacturaResumen() {
@@ -113,9 +113,9 @@ public class Imprimir {
         double aux = 0;
         // calcula subtotal otra vez igual que en imprimirFacturaCompleta
         int i = 0;
-        while (i < Datos.nombres.length) {
-            if (Datos.cantidades[i] > 0) {
-                sub = sub + Datos.precios[i] * Datos.cantidades[i];
+        while (i < Carta.nombres.length) {
+            if (PedidoActual.cantidades[i] > 0) {
+                sub = sub + Carta.precios[i] * PedidoActual.cantidades[i];
                 cont = cont + 1;
             }
             i++;
@@ -141,7 +141,7 @@ public class Imprimir {
         System.out.println("    Calle 15 #8-32, Valledupar");
         System.out.println("    NIT: 900.123.456-7");
         System.out.println(sep);
-        System.out.printf("FACTURA No. %03d (RESUMEN)%n", Datos.numeroFactura);
+        System.out.printf("FACTURA No. %03d (RESUMEN)%n", Carta.numeroFactura);
         System.out.println("----------------------------------------");
         System.out.printf("%-27s $%,.0f%n", "Subtotal:", aux);
         System.out.printf("%-27s $%,.0f%n", "IVA (19%):", iva);
