fc7b46c2ce0117ab4b30fdb53a43c50a95fe10d1
yjosuesilva-code <yjosuesilva@unicesar.edu.co>
2026-04-16 00:44:06 -0500

    refactor: Se dividio la clase Datos con la clase PedidoActual y se elimino la clase Datos

--- RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/PedidoActual.java
+++ RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/PedidoActual.java
@@ -1,4 +1,7 @@
 package com.mycompany.restauranteelbuensabor;
 
 public class PedidoActual {
+    public static int[] cantidades = {0, 0, 0, 0, 0, 0, 0, 0};
+    public static double total = 0;
+    public static String tmp = "";
 }
