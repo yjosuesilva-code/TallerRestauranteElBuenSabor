58ba0d34a1d8833aa1d084d814572cfe4d370bee
yjosuesilva-code <yjosuesilva@unicesar.edu.co>
2026-04-15 23:34:19 -0500

    refactor: se cambio el formato a todos las clases

--- RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Datos.java
+++ RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Datos.java
@@ -9,11 +9,13 @@ package com.mycompany.restauranteelbuensabor;
  * @author alfre
  */
 public class Datos {
-public static String[] nom={"Bandeja Paisa","Sancocho de Gallina","Arepa con Huevo","Jugo Natural","Gaseosa","Cerveza Poker","Agua Panela","Arroz con Pollo"};
-public static double[] p={32000,28000,8000,7000,4500,6000,3500,25000};
-public static int[] cant={0,0,0,0,0,0,0,0};
-public static int ms=0;public static int est=0;
-public static double tot=0;public static int nf=1;
-public static String x="El Buen Sabor";
-public static String tmp="";    
+    public static String[] nom = {"Bandeja Paisa", "Sancocho de Gallina", "Arepa con Huevo", "Jugo Natural", "Gaseosa", "Cerveza Poker", "Agua Panela", "Arroz con Pollo"};
+    public static double[] p = {32000, 28000, 8000, 7000, 4500, 6000, 3500, 25000};
+    public static int[] cant = {0, 0, 0, 0, 0, 0, 0, 0};
+    public static int ms = 0;
+    public static int est = 0;
+    public static double tot = 0;
+    public static int nf = 1;
+    public static String x = "El Buen Sabor";
+    public static String tmp = "";
 }
