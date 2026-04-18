9fd169e581b1112d5b26efae925cd19cd4526985
yjosuesilva-code <yjosuesilva@unicesar.edu.co>
2026-04-18 02:20:20 -0500

    refactor(PedidoActual): se elimino variable sin uso

--- RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/PedidoActual.java
+++ RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/PedidoActual.java
@@ -3,5 +3,4 @@ package com.mycompany.restauranteelbuensabor;
 public class PedidoActual {
     public static int[] cantidades = {0, 0, 0, 0, 0, 0, 0, 0};
     public static double total = 0;
-    public static String tmp = "";
 }
