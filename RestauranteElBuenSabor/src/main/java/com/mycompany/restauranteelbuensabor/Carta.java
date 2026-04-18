a74f593fa492d9b657b53919f7c33943e8d4655b
yjosuesilva-code <yjosuesilva@unicesar.edu.co>
2026-04-18 14:17:37 -0500

    refactor(Carta): se elimino los ArrayList paralelos y se agrego ArrayLis de Producto y se agregaron Getters y Setters

--- RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Carta.java
+++ RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Carta.java
@@ -1,9 +1,33 @@
 package com.mycompany.restauranteelbuensabor;
 
 public class Carta {
+
     public static final String NOMBRE_RESTAURANTE = "El Buen Sabor";
     public static final String DIRECCION          = "Calle 15 #8-32, Valledupar";
     public static final String NIT                = "900.123.456-7";
-    public static String[] nombres = {"Bandeja Paisa", "Sancocho de Gallina", "Arepa con Huevo", "Jugo Natural", "Gaseosa", "Cerveza Poker", "Agua Panela", "Arroz con Pollo"};
-    public static double[] precios = {32000, 28000, 8000, 7000, 4500, 6000, 3500, 25000};
-    public static int numeroFactura = 1;}
+
+    private static final Producto[] productos = {
+            new Producto("Bandeja Paisa",       32000),
+            new Producto("Sancocho de Gallina", 28000),
+            new Producto("Arepa con Huevo",      8000),
+            new Producto("Jugo Natural",          7000),
+            new Producto("Gaseosa",               4500),
+            new Producto("Cerveza Poker",         6000),
+            new Producto("Agua Panela",           3500),
+            new Producto("Arroz con Pollo",      25000)
+    };
+
+    public static int numeroFactura = 1;
+
+    public static Producto[] getProductos() {
+        return productos;
+    }
+
+    public static int cantidadProductos() {
+        return productos.length;
+    }
+
+    public static Producto getProducto(int indice) {
+        return productos[indice];
+    }
+}
